c262b592f3e65e98ec051a391a4ce69ad5a73c04
peter-akworo <pittonyango@gmail.com>
2020-05-10 00:31:17 +0300

    select where
    Result set constraints

--- PostgreSQL/Scripts/Selection.sql
+++ PostgreSQL/Scripts/Selection.sql
@@ -8,5 +8,7 @@ select distinct on(title) title, tape_id from tapes;--filter out rows that have 
 select * from customers where balance > 0;--retrieve rows where column(balance) is more than 0
 select customer_name, phone from customers where (balance = 0) and (age(birth_date) < '50 years');--retrieve rows that meet the two column constraints
 select customer_name, phone from customers where (balance = 0) or (age(birth_date) < '50 years');--retrieve rows that meet either of the two column constraints
+select * from customers where not(balance = 0);--retrieve rows from table customers where balance is not 0
+select customer_id, customer_name from customers where balance != 0;--constraint doesn't have to be part of the result set
 
 

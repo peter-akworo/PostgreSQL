301242aec36c3cd2c8e6723ed27bd6180ed2a017
peter-akworo <pittonyango@gmail.com>
2020-05-09 19:36:00 +0300

    select column list

--- PostgreSQL/Scripts/Script.sql
+++ PostgreSQL/Scripts/Script.sql
@@ -1,2 +1,3 @@
 select * from customers;--retrieve all columns from table customers
 select title from tapes;--retrieve single column(title) from table tapes
+select customer_name, birth_date from customers;--retrieve column list from table customers

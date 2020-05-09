d7f356b4ad9968ef99c889cafed6cbe31f965d49
peter-akworo <pittonyango@gmail.com>
2020-05-09 20:09:15 +0300

    Select distinct
    filters out duplicate rows from the result set

--- PostgreSQL/Scripts/Selection.sql
+++ PostgreSQL/Scripts/Selection.sql
@@ -2,3 +2,4 @@ select * from customers;--retrieve all columns from table customers
 select title from tapes;--retrieve single column(title) from table tapes
 select customer_name, birth_date from customers;--retrieve column list from table customers
 select customer_name, birth_date, age(birth_date) from customers;--retrieve column list and expression(age) from table customers
+select distinct title from tapes;--filters out duplicate rows from the result set

ba1551d4bd09547fa3094ef464cbb29d9ce683ef
peter-akworo <pittonyango@gmail.com>
2020-05-09 20:14:37 +0300

    select distinct on()
    filter out rows that have duplicate values in one or more columns

--- PostgreSQL/Scripts/Selection.sql
+++ PostgreSQL/Scripts/Selection.sql
@@ -3,3 +3,5 @@ select title from tapes;--retrieve single column(title) from table tapes
 select customer_name, birth_date from customers;--retrieve column list from table customers
 select customer_name, birth_date, age(birth_date) from customers;--retrieve column list and expression(age) from table customers
 select distinct title from tapes;--filters out duplicate rows from the result set
+select title, tape_id from tapes;--retrieve two columns duplicate on one(title) distinct on another(tape_id)
+select distinct on(title) title, tape_id from tapes;--filter out rows that have duplicate values in one or more columns
